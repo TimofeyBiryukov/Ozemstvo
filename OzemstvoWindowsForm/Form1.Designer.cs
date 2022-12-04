f23f07a14b5a8bad306c2131ad3904cc8a2815c0
Timofey Biryukov <timofeybiryukov@gmail.com>
2022-12-04 16:48:39 +0600

    add basic settings

--- OzemstvoWindowsForm/Form1.Designer.cs
+++ OzemstvoWindowsForm/Form1.Designer.cs
@@ -28,8 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Foo");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Bar");
             this.ClickMe = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
             this.SuspendLayout();
             // 
             // ClickMe
@@ -51,11 +57,45 @@
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Items.AddRange(new object[] {
+            "Foo",
+            "Bar"});
+            this.listBox1.Location = new System.Drawing.Point(15, 83);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            listViewGroup1.Header = "ListViewGroup";
+            listViewGroup1.Name = "listViewGroup1";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1});
+            listViewItem1.Group = listViewGroup1;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+            this.listView1.Location = new System.Drawing.Point(15, 199);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(233, 183);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.ClickMe);
             this.Name = "Form1";
@@ -70,5 +110,8 @@
 
         private Button ClickMe;
         private TextBox textBox1;
+        private ListBox listBox1;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
     }
 }
--- OzemstvoWindowsForm/Form1.cs
+++ OzemstvoWindowsForm/Form1.cs
@@ -1,3 +1,5 @@
+using System.Configuration;
+
 namespace OzemstvoWindowsForm
 {
     public partial class Form1 : Form
@@ -8,21 +10,38 @@ namespace OzemstvoWindowsForm
         {
             InitializeComponent();
             ozemstvo.Init();
+            // var foo = Properties.Settings.Default.Foo;
+            // Console.WriteLine(foo);
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //foreach (SettingsProperty settingsProperty in Properties.Rules.Default.Properties)
+            //{
+            //    textBox1.Text = settingsProperty.Name;
+            //}
+            textBox1.Text = Properties.Rules.Default.Properties.Count.ToString();
 
         }
 
         private void ClickMe_Click(object sender, EventArgs e)
         {
-            // get value of textBox1
             var url = textBox1.Text;
             ozemstvo.Run(new Uri(url));
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
