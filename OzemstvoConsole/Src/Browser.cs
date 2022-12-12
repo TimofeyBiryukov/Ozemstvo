c6fa4db8f6ec2f67f6715d25f03e700fc3e67324
Timofey Biryukov <timofeybiryukov@gmail.com>
2022-12-12 21:42:17 +0600

    browser id

--- OzemstvoConsole/Src/Browser.cs
+++ OzemstvoConsole/Src/Browser.cs
@@ -2,6 +2,7 @@
 
 public class Browser
 {
+  public string Id = Guid.NewGuid().ToString();
   public string Name { get; set; }
   public string Path { get; set; }
   public bool Default { get; set; } = false;
