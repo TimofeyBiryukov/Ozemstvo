988b1d2b9a369219bfc803e090e9ee8014ab7351
Timofey Biryukov <timofeybiryukov@gmail.com>
2022-12-26 11:33:58 +0600

    rp refactor

--- OzemstvoWPF/MainWindow.xaml.cs
+++ OzemstvoWPF/MainWindow.xaml.cs
@@ -11,17 +11,6 @@ using OzemstvoWPF.Controls;
 
 namespace OzemstvoWPF
 {
-    public class RuleProperty
-    {
-        virtual public string? Id { get; set; } = null;
-        virtual public string Name { get; set; } = string.Empty;
-        virtual public string Browser { get; set; } = string.Empty;
-        virtual public string Data { get; set; } = string.Empty;
-        virtual public string Template { get; set; } = "{{url}}";
-        virtual public string Type { get; set; } = string.Empty;
-        virtual public string Example { get; set; } = string.Empty;
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
---
+++ OzemstvoWPF/RuleProperty.cs
@@ -0,0 +1,13 @@
+﻿namespace OzemstvoWPF
+{
+    public class RuleProperty
+    {
+        virtual public string? Id { get; set; } = null;
+        virtual public string Name { get; set; } = string.Empty;
+        virtual public string Browser { get; set; } = string.Empty;
+        virtual public string Data { get; set; } = string.Empty;
+        virtual public string Template { get; set; } = "{{url}}";
+        virtual public string Type { get; set; } = string.Empty;
+        virtual public string Example { get; set; } = string.Empty;
+    }
+}
