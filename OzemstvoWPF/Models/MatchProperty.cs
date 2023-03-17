cf082334fdca936441fd8db192f9480846f72c37
Timofey Biryukov <timofeybiryukov@gmail.com>
2023-03-17 18:18:48 +0600

    random color selector

--- OzemstvoWPF/Models/MatchProperty.cs
+++ OzemstvoWPF/Models/MatchProperty.cs
@@ -1,4 +1,6 @@
-﻿namespace OzemstvoWPF.Models;
+﻿using System;
+
+namespace OzemstvoWPF.Models;
 
 public class MatchProperty
 {
@@ -6,4 +8,31 @@ public class MatchProperty
     virtual public string Value { get; set; } = string.Empty;
     virtual public string Type { get; set; } = string.Empty;
     virtual public string Color { get; set; } = "";
+
+    public MatchProperty()
+    {
+        if (string.IsNullOrEmpty(Color))
+        {
+            var random = new Random();
+            var number = random.Next(1, 6);
+            switch (number)
+            {
+                case 1:
+                    Color = "#151F30";
+                    break;
+                case 2:
+                    Color = "#103778";
+                    break;
+                case 3:
+                    Color = "#0593A2";
+                    break;
+                case 4:
+                    Color = "#FF7A48";
+                    break;
+                case 5:
+                    Color = "#E3371E";
+                    break;
+            }
+        }
+    }
 }
--- OzemstvoWPF/Models/RuleProperty.cs
+++ OzemstvoWPF/Models/RuleProperty.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 
 namespace OzemstvoWPF.Models
 {
@@ -11,5 +12,32 @@ namespace OzemstvoWPF.Models
         virtual public string Template { get; set; } = "{{url}}";
         virtual public string Example { get; set; } = string.Empty;
         virtual public string Color { get; set; } = "";
+
+        public RuleProperty()
+        {
+            if (string.IsNullOrEmpty(Color))
+            {
+                var random = new Random();
+                var number = random.Next(1, 6);
+                switch (number)
+                {
+                    case 1:
+                        Color = "#151F30";
+                        break;
+                    case 2:
+                        Color = "#103778";
+                        break;
+                    case 3:
+                        Color = "#0593A2";
+                        break;
+                    case 4:
+                        Color = "#FF7A48";
+                        break;
+                    case 5:
+                        Color = "#E3371E";
+                        break;
+                }
+            }
+        }
     }
 }
